b1fc806462bdfa74b7ec520911004a1a15891723
steph44470 <skrechowiec@gmail.com>
2017-04-03 17:53:40 +0200

    ajout des 1ers fichiers

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Les meilleures combinaisons au SummerGame</title>
+	<head>
+	
+	<body>
+		<div id="bloc_page">
+			<header>
+				<div id="titre_principal">
+					<div id="logo">
+						<img src="images/logo.png" alt="le logo" />
+						<h1>Les meilleures combinaisons au SummerGame</h1>
+					</div>
+					<h2>Comment trouver la meuilleur combinaisons au SummerGame ?</h2>
+				</div>
+				
+				<nav>
+					<ul>
+						<li><a href="#">Accueil</a></li>
+						<li><a href="#">Tête</a></li>
+						<li><a href="#">Haut</a></li>
+						<li><a href="#">Bas</a></li>
+					</ul>
+				</nav>
+			</header>
+			
+			<div id="baniere_image">
+				<div id="baniere_description">
+					<a href="#" class="bouton_rouge">Voir l'article <img src="images/flecheblanchedroite.png" alt="" /> </a>
+				</div>
+			</div>
+			
+			<section>
+				<article>
+					<h1><img src="images/ico_epingle.png" alt="explication du site" class="explication_site" />Explication du site</h1>
+					<p>Ce site permet de calculer la meilleur combinaison au jeu SummerGame</p>
+                    <p>Vivamus sed libero nec mauris pulvinar facilisis ut non sem. Quisque mollis ullamcorper diam vel faucibus. 
+					Vestibulum sollicitudin facilisis feugiat. Nulla euismod sodales hendrerit. Donec quis orci arcu. Vivamus fermentum magna a erat ullamcorper dignissim pretium nunc aliquam. 
+					Aenean pulvinar condimentum enim a dignissim. Vivamus sit amet lectus at ante adipiscing adipiscing eget vitae felis. In at fringilla est. Cras id velit ut magna rutrum commodo.
+					Etiam ut scelerisque purus. Duis risus elit, venenatis vel rutrum in, imperdiet in quam. Sed vestibulum, libero ut bibendum consectetur, eros ipsum ultrices nisl, in rutrum diam augue non tortor.
+					Fusce nec massa et risus dapibus aliquam vitae nec diam.</p>
+                    <p>Phasellus ligula massa, congue ac vulputate non, dignissim at augue. Sed auctor fringilla quam quis porttitor. 
+					Praesent vitae dignissim magna. Pellentesque quis sem purus, vel elementum mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+					Maecenas consectetur euismod urna. In hac habitasse platea dictumst. Quisque tincidunt porttitor vestibulum. Ut iaculis, lacus at molestie lacinia, ipsum mi adipiscing ligula, vel mollis sem risus eu lectus. 
+					Nunc elit quam, rutrum ut dignissim sit amet, egestas at sem.</p>
+				</article>
+				<aside>
+					<h1>A propos de l'auteur</h1>
+					<img src="images/bulle.png" alt="" id="fleche_bulle" />
+					<p id="photo_moi"><img src="images/photo_moi.png" alt="Photo de moi" /></p>
+					<p>Chargé d'assistance chez Sigma</p>
+					<p>Equipe TAAP</p>
+					<p><img href="www.facebook.fr" src="images/facebook.png" alt="facebook" />
+					<img href="www.twitter.fr" src="images/twitter.png" alt="twitter" />
+					<img href="www.vimeo.fr" src="images/vimeo.png" alt="vimeo" />
+					<img href="www.flicker.fr" src="images/flickr.png" alt="flickr" />
+				</aside>
+			</section>
+			
+			<footer>
+				<div id="note">
+					<h1>Ma derniere note</h1>
+					<p>derniere modification</p>
+					<p>30/03/2017</p>
+				</div>
+				<div id="mes_photos">
+					<p><img src="images/captain.png" alt="captain america" />
+					<img src="images/hulk.png" alt="hulk" />
+					<img src="images/thor.png" alt="thor" />
+					<img src="images/cyclop.png" alt="cyclop" />
+				</div>
+				<div id="mes_amis">
+					<h1>mes amis H1</h1>
+					<div id="liste_ami">
+						<ul>
+							<li><a href="#">Poupi</a></li>
+							<li><a href="#">test</a></li>
+							<li><a href="#">david</a></li>
+						</ul>
+						<ul>
+							<li><a href="#">toto</a></li>
+							<li><a href="#">titi</a></li>
+							<li><a href="#">tutu</a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>	
+		</div>
+	</body>	
+</html>				
+				
+				
